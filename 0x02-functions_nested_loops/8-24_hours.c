5db8c32b567e66098c85db0fb1a54046054147d7
ogunleye james babatunde <tunde200.james@gmail>
2022-09-13 16:37:30 -0700

    my new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@
 void jack_bauer(void)
 {
 int i, j;
-for (i = 0; i < 24; 1++)
+for (i = 0; i < 24; i++)
 {
-for (j = 0; j < 60; J++)
+for (j = 0; j < 60; j++)
 {
 	_putchar(i / 10 + 48);
 	_putchar(i % 10 + 48);
