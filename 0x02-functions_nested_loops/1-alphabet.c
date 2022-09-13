e5df2b20267eba6aa1e1964cec68c0c1d974d48e
ogunleye james babatunde <tunde200.james@gmail>
2022-09-13 09:35:32 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+int alph;
+for (alph = 'a'; alpha <= 'z'; alpha++)
+	_putchar(alpha);
+_putchar('\n');
+return (0);
+}
