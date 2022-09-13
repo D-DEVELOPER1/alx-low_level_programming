13fc7d4e141c80a8dfe1cc2d1066a4338dfbf6f2
ogunleye james babatunde <tunde200.james@gmail>
2022-09-13 15:52:22 -0700

    my new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of the integer
+ * @n: int type number
+ */
+int _abs(int)
+{
+if (n < 0)
+{
+	return (n * -1);
+}
+else
+{
+	return (0);
+}
+}
