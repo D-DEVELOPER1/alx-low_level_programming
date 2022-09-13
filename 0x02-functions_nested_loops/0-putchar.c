284b3516e8bcf251a8278f89506c166b40e03c36
ogunleye james babatunde <tunde200.james@gmail>
2022-09-13 08:07:16 -0700

    this is my new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+putchar("_putchar");
+putchar('\n');
+return (0);
+}
