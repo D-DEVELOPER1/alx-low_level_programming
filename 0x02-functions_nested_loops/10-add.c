85c47c2f01fa802e33f14a52bd693fb4ccce997a
ogunleye james babatunde <tunde200.james@gmail>
2022-09-13 16:52:46 -0700

    my new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two number from input
+ * @a: first aparamet
+ * @b: second parameter
+ *
+ * Return: Always (0).
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
