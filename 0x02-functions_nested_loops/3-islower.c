6833aadcb679a312e733d91060d4bfa290755ade
ogunleye james babatunde <tunde200.james@gmail>
2022-09-13 13:10:01 -0700

    my new commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - printe 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Return: Always(0)
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
