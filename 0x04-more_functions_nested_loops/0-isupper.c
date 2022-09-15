848bce25f7d8b4a5bd62397c47f4b409c8eed33d
ogunleye james babatunde <tunde200.james@gmail>
2022-09-15 08:59:21 -0700

    this is my new commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase characte
+ * @c: checking for uppercase
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+int c;
+if (isupper(c))
+{
+	_putchar('1');
+}
+else
+{
+	_putchar('0');
+}
+return (0);
+}
