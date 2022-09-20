58da61f2ee4c971d39da25bb0ae19c20e03ce71d
ogunleye james babatunde <tunde200.james@gmail>
2022-09-19 19:20:37 -0700

    my new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - prints in reversed
+ * @str: string to be reversed
+ * Return: Null
+ */
+void print_rev(char *str)
+{
+int i, len = strlen(str);
+for (i = len - 1; i >= 0; i--)
+	_putchar (str[i]);
+_putchar('\n');
+}
