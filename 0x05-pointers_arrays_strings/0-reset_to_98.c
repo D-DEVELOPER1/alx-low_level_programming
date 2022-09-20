4b0eb69c666b59afc79264009dfe562e09ff1d71
ogunleye james babatunde <tunde200.james@gmail>
2022-09-19 17:55:28 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * reset_to_98 - take the pointer to a variable and change it value
- * @n: value to be change
- */
-void reset_to_98(int *n)
-{
-*n = 98;
-}
--- 0x05-pointers_arrays_strings/main.h
+++
@@ -1,4 +0,0 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-int _putchar(char);
-void reset_to_98(int *n);
