66b07895df072f7994debb9ab4c8bea9879ddcaa
ogunleye james babatunde <tunde200.james@gmail>
2022-10-05 08:04:43 -0700

    my new commit

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - the main description
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	_puts("\"my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
